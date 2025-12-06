7c05492b15dc3f68e50e13df286b9281e8d74130
MrKuothe <xabitic1@gmail.com>
2025-12-06 01:24:09 +0100

    v1

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -2,11 +2,23 @@
 
 # Iniciar los contenedores con Docker Compose
 echo "Iniciando el clúster Galera..."
-docker compose up -d
+docker-compose up -d
 
-# Esperar unos segundos para que los contenedores se inicien
-echo "Esperando a que los contenedores se inicien..."
-sleep 10
+# Función para verificar si el contenedor está en ejecución
+wait_for_container() {
+  local container_name=$1
+  echo "Esperando a que el contenedor $container_name esté en ejecución..."
+  while ! docker ps --filter "name=$container_name" --format '{{.Names}}' | grep -q "$container_name"; do
+    sleep 1
+  done
+  echo "$container_name está en ejecución."
+}
+
+# Esperar a que los contenedores se inicien
+wait_for_container "mysql-galera-cluster_mysql-node1_1"
+wait_for_container "mysql-galera-cluster_mysql-node2_1"
+wait_for_container "mysql-galera-cluster_mysql-node3_1"
+wait_for_container "mysql-galera-cluster_haproxy_1"
 
 # Configurar el primer nodo (mysql-node1) como nodo inicial
 echo "Configurando el primer nodo (mysql-node1) como nodo inicial..."
@@ -19,7 +31,7 @@ sleep 10
 # Configurar los otros nodos (mysql-node2 y mysql-node3) para unirse al clúster
 echo "Configurando los nodos restantes para unirse al clúster..."
 docker exec -it mysql-galera-cluster_mysql-node2_1 mysql -u root -proot -e "SET GLOBAL wsrep_cluster_address='gcomm://mysql-node1';"
-docker exec -it mysql-galera-cluster_mysql-node3_1 mysql -u root -prootd -e "SET GLOBAL wsrep_cluster_address='gcomm://mysql-node1';"
+docker exec -it mysql-galera-cluster_mysql-node3_1 mysql -u root -proot -e "SET GLOBAL wsrep_cluster_address='gcomm://mysql-node1';"
 
 # Esperar unos segundos para que todos los nodos se conecten
 echo "Esperando a que todos los nodos se conecten..."
